db23818d5994d9fe4550c7ef675f42e46a019bb0
Yahya Murad <yahyamurad197@gmail.com>
2025-11-01 01:34:51 +0300

    update table structure

--- app/models/models.py
+++ app/models/models.py
@@ -1,8 +1,11 @@
+import datetime
+
 from sqlalchemy import (
     JSON,
     TIMESTAMP,
     Boolean,
     Column,
+    DateTime,
     ForeignKey,
     Integer,
     String,
@@ -18,10 +21,33 @@ class User(Base):
     id = Column(Integer, primary_key=True, index=True)
     username = Column(String, unique=True, nullable=False)
     email = Column(String, unique=True, nullable=False)
+    password_hash = Column(String, nullable=False)
 
-    machines = relationship("Machine", back_populates="user")
-    heartbeats = relationship("Heartbeat", back_populates="user")
-    daily_activities = relationship("DailyActivity", back_populates="user")
+    machines = relationship(
+        "Machine", back_populates="user", cascade="all, delete-orphan"
+    )
+    heartbeats = relationship(
+        "Heartbeat", back_populates="user", cascade="all, delete-orphan"
+    )
+    daily_activities = relationship(
+        "DailyActivity", back_populates="user", cascade="all, delete-orphan"
+    )
+    api_keys = relationship(
+        "APIKey", back_populates="user", cascade="all, delete-orphan"
+    )
+
+
+class APIKey(Base):
+    __tablename__ = "api_keys"
+
+    id = Column(Integer, primary_key=True, index=True)
+    user_id = Column(Integer, ForeignKey("users.id"), nullable=False)
+    key = Column(String, unique=True, index=True, nullable=False)
+    created_at = Column(DateTime, default=datetime.datetime.now(datetime.UTC).date())
+    is_active = Column(Boolean, default=True)
+
+    # Relationships
+    user = relationship("User", back_populates="api_keys")
 
 
 class Machine(Base):
@@ -31,8 +57,11 @@ class Machine(Base):
     user_id = Column(Integer, ForeignKey("users.id"), nullable=False)
     machine_name = Column(String, nullable=False)
 
+    # Relationships
     user = relationship("User", back_populates="machines")
-    heartbeats = relationship("Heartbeat", back_populates="machine")
+    heartbeats = relationship(
+        "Heartbeat", back_populates="machine", cascade="all, delete-orphan"
+    )
 
 
 class Heartbeat(Base):
@@ -41,6 +70,7 @@ class Heartbeat(Base):
     id = Column(Integer, primary_key=True, index=True)
     user_id = Column(Integer, ForeignKey("users.id"), nullable=False)
     machine_id = Column(Integer, ForeignKey("machines.id"), nullable=True)
+
     entity = Column(String, nullable=False)
     type = Column(String)
     category = Column(String)
@@ -53,6 +83,7 @@ class Heartbeat(Base):
     cursorpos = Column(Integer)
     is_write = Column(Boolean, default=False)
 
+    # Relationships
     user = relationship("User", back_populates="heartbeats")
     machine = relationship("Machine", back_populates="heartbeats")
 
@@ -64,6 +95,7 @@ class DailyActivity(Base):
     user_id = Column(Integer, ForeignKey("users.id"), nullable=False)
     date = Column(TIMESTAMP, nullable=False)
     total_active_seconds = Column(Integer, default=0)
-    project_worked_on = Column(JSON)
+    project_worked_on = Column(JSON, default={})
 
+    # Relationship
     user = relationship("User", back_populates="daily_activities")
