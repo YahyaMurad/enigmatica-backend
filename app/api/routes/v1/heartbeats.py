0e3860e27bb5f01c76584a874715fccb7c7dc03f
Yahya Murad <yahyamurad197@gmail.com>
2025-11-04 00:34:39 +0300

    modify existing api route

--- app/api/routes/v1/heartbeats.py
+++ app/api/routes/v1/heartbeats.py
@@ -1,10 +1,16 @@
 from typing import List
+
 from fastapi import APIRouter, Depends
 from sqlalchemy.orm import Session
 
+from app.core.dependencies import (
+    get_current_user_id,
+    get_current_user_id_from_api_key,
+    get_db,
+)
 from app.models.models import Heartbeat
 from app.schemas.heartbeats import HeartbeatCreate, HeartbeatOut
-from app.core.dependencies import get_current_user_id, get_current_user_id_from_api_key, get_db
+
 router = APIRouter(prefix="/api/v1/heartbeats", tags=["heartbeats"])
 
 
@@ -14,7 +20,7 @@ router = APIRouter(prefix="/api/v1/heartbeats", tags=["heartbeats"])
 @router.get("/", response_model=List[HeartbeatOut])
 async def get_heartbeats(
     db: Session = Depends(get_db),
-    current_user_id: int = Depends(get_current_user_id)
+    current_user_id: int = Depends(get_current_user_id),
 ):
     heartbeats = db.query(Heartbeat).filter(Heartbeat.user_id == current_user_id).all()
     return heartbeats
@@ -23,14 +29,20 @@ async def get_heartbeats(
 # -----------------------
 # POST /api/v1/heartbeats
 # -----------------------
-@router.post("/", response_model=HeartbeatOut)
-async def create_heartbeat(
-    hb: HeartbeatCreate,
+@router.post("/", response_model=List[HeartbeatOut])
+async def create_heartbeats(
+    heartbeats: List[HeartbeatCreate],
     db: Session = Depends(get_db),
     current_user_id: int = Depends(get_current_user_id_from_api_key),
 ):
-    new_heartbeat = Heartbeat(**hb.dict(), user_id=current_user_id)
-    db.add(new_heartbeat)
+    new_heartbeats = [
+        Heartbeat(**hb.model_dump(), user_id=current_user_id) for hb in heartbeats
+    ]
+
+    db.add_all(new_heartbeats)
     db.commit()
-    db.refresh(new_heartbeat)
-    return new_heartbeat
+
+    for hb in new_heartbeats:
+        db.refresh(hb)
+
+    return new_heartbeats
