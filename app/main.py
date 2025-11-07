112de7b77081ba2db03940fc3ba8f84cad5814f7
Yahya Murad <yahyamurad197@gmail.com>
2025-11-07 22:14:47 +0300

    add endpoints to main file

--- app/main.py
+++ app/main.py
@@ -1,9 +1,12 @@
 from fastapi import FastAPI
 
-from app.api.routes.v1 import heartbeats
+from app.api.routes.v1 import aggregator, auth, heartbeats
 
 app = FastAPI(title="Enigmatica API")
+
+app.include_router(auth.router, tags=["V1"])
 app.include_router(heartbeats.router, tags=["V1"])
+app.include_router(aggregator.router, tags=["V1"])
 
 
 @app.get("/")
